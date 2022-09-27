5c153a56b526afbe46014e6c8048d6b4f0c6a5d9
ESCG\530008 <530008@chrome.escg.ac.uk>
2022-09-27 16:39:42 +0100

    dialoge

--- Program.cs
+++ Program.cs
@@ -5,12 +5,50 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("𝘸𝘩𝘦𝘳𝘦 𝘢𝘮 𝘐? 𝘯𝘰 𝘴𝘤𝘳𝘢𝘱 𝘵𝘩𝘢𝘵, 𝘸𝘩𝘰 𝘢𝘮 𝘐?");
-        Console.WriteLine("As the world slowly adorns my eyes i try to survey my surrondings in hope of finding a answer, i seem to be sat up against a wall in a ruined alleyway with what seems like crystals sticking out from the crubleling buildings. A small girl with pink hair is perched next ");
-        double speed = 10;
-        double pwr = 10;
-        Console.WriteLine("");
-        Console.WriteLine("beside you rests a light looking sword and a heavy looking h"
+        bool playactive = true;
+        while (playactive == true)
+        {
+            int playthrough = 0;
+            double speed = 0;
+            double pwr = 0;
+            double epower = 0;
+            double espeed = 0;
+            if (playthrough == 0)
+            {
+                Console.WriteLine("𝘸𝘩𝘦𝘳𝘦 𝘢𝘮 𝘐? 𝘯𝘰 𝘴𝘤𝘳𝘢𝘱 𝘵𝘩𝘢𝘵, 𝘸𝘩𝘰 𝘢𝘮 𝘐?");
+            }
+            else if (playthrough == 1)
+            {
+                Console.WriteLine("where am I?  no scrap that, who am I?");
+            }
+            else
+            {
+                Console.WriteLine("why do i know where i am?");
+            }
+
+            Console.WriteLine("As the world slowly adorns my eyes i try to survey my surrondings in hope of finding a answer, i seem to be sat up against a wall in a ruined alleyway with what seems like crystals sticking out from the crubleling buildings. A small girl with pink hair is perched next to me looking at me inquisitively");
+            if (playthrough == 0)
+            {
+                speed = 10;
+                pwr = 10;
+            }
+            else
+            {
+                pwr = 5;
+                speed = 5;
+            }
+            Console.WriteLine(" Girl: ah! you have awakend at last! i was getting worried! ");
+            Console.WriteLine(" You: who are you? who even am i? where the hell are we? ");
+            Console.WriteLine(@"Girl: im inori you are 'leave' as for anything else i am unsure thats all i can rememeber, however you probably want to grab one of those wepons ive seen what looks like monsters walking about");
+            if (playthrough == 0)
+            {
+                Console.WriteLine("beside you rests ");
+            }
+            playactive = false;
+        }
+        playactive = true;
     }
-}
-}
+    }
+
+        
+
